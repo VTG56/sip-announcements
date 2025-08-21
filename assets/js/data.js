c516a594b34c1f7723756980c599320b9d7ca3e3
VTG56 <vishwaradhya.aihole@gmail.com>
2025-08-22 02:10:58 +0530

    Initial commit

---
+++ assets/js/data.js
@@ -0,0 +1,33 @@
+// Sample data - just the main 5 categories as individual items
+window.ANNOUNCEMENTS = [
+  {
+    title: "Circulars",
+    date: "21 Aug 2025",
+    driveShare: "https://drive.google.com/file/d/1AbcDEF234567890fakeId1/view?usp=sharing",
+    type: "circulars"
+  },
+  {
+    title: "Bootkit",
+    date: "20 Aug 2025", 
+    driveShare: "https://drive.google.com/file/d/1XyzGHI234567890fakeId2/view?usp=sharing",
+    type: "bootkit"
+  },
+  {
+    title: "Counselor Details",
+    date: "19 Aug 2025",
+    driveShare: "https://drive.google.com/open?id=1QwePQR234567890fakeId3",
+    type: "counselors"
+  },
+  {
+    title: "Venues",
+    date: "18 Aug 2025",
+    driveShare: "https://drive.google.com/file/d/1MnoSTU234567890fakeId4/view?usp=sharing",
+    type: "venues"
+  },
+  {
+    title: "Club Showcasing", 
+    date: "17 Aug 2025",
+    driveShare: "https://drive.google.com/uc?id=1VwxYZA234567890fakeId5&export=download",
+    type: "clubs"
+  }
+];
